32320d030eb7c773a73ded8bb26a6ed81e9c6efb
Craig Petterson <cpetterson@yozu.co.uk>
2019-09-23 16:07:33 +0100

    Restore purgecss

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   content: ["./_site/**/*.html"],
   css: ["./_site/assets/site.css"],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+
+  // Tailwind CSS config
+  extractors: [{
+    extractor: class {
+      static extract(content) {
+        return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+      }
+    },
+    extensions: ["html"]
+  }]
 };
